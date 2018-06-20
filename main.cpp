f3dd1f322ee994d736be7a4838e02d9199ca997e
frozenbanana <henrybergstrom1@gmail.com>
2018-06-20 15:33:42 +0200

    Added camera class

--- main.cpp
+++ main.cpp
@@ -3,14 +3,18 @@
 #include "sphere.h"
 #include "hitable_list.h"
 #include "float.h"
+#include "camera.h"
 
 vec3 color(const ray &r, hitable *world)
 {
     // First check if ray hits hitable object
-    hit_record rec; 
-    if (world->hit(r, 0.0, MAXFLOAT, rec)) {
-        return 0.5*vec3(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
-    } else { 
+    hit_record rec;
+    if (world->hit(r, 0.0, MAXFLOAT, rec))
+    {
+        return 0.5 * vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
+    }
+    else
+    {
         // Getting normalized direction of ray
         vec3 dir = unit_vector(r.direction());
 
@@ -26,8 +30,9 @@ int main()
 {
     int nx = 200;
     int ny = 100;
-    std::cout << "P3\n"
-              << nx << " " << ny << "\n255" << std::endl;
+    int ns = 100; // nr of samples for anti-aliasing
+    std::cout << "P3\n" << nx << " " << ny << "\n255" << std::endl;
+
     // Specification of camera and the quad
     vec3 lower_left_corner(-2.0, -1.0, -1.0);
     vec3 horizontal(4.0, 0.0, 0.0);
@@ -40,17 +45,24 @@ int main()
 
     hitable *world = new hitable_list(list, 2);
 
+    camera cam;
     for (int j = ny - 1; j >= 0; j--)
     {
         for (int i = 0; i < nx; i++)
         {
-            // Getting UV coordinates
-            float u = float(i) / float(nx);
-            float v = float(j) / float(ny);
-            // Create corresponding ray
-            ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-            // Get color where ray is hitting quad
-            vec3 col = color(r, world);
+            vec3 col(0,0,0);
+            for (int s = 0; s < ns; s++)
+            {
+                // Getting UV coordinates
+                float u = float(i + drand48()) / float(nx);
+                float v = float(j + drand48()) / float(ny);
+                // Create corresponding ray
+                ray r = cam.get_ray(u,v);
+                // Get color where ray is hitting quad
+                col += color(r, world);
+            }
+            col /= float(ns); // get average color from samples ns
+
             float ir = int(255.99 * col[0]);
             float ig = int(255.99 * col[1]);
             float ib = int(255.99 * col[2]);
